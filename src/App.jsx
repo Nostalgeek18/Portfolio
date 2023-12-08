9a11edd6087e2f66a887c6811a22fff059c0e3c5
Nazim Mouzai <choco18@live.ca>
2023-12-08 00:48:31 -0500

    improving reusability

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,68 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'
+import { Header, Logo, Navigation, HamburgerMenu, SmallMenu } from './components/Header';
 import Footer from './components/Footer'
 import Projects from './components/Projects'
 import About from './components/About'
 import Contact from './components/Contact'
-import Home from './components/Home'
+import {Home, HeroSocials, HeroSocial, HomeContent, HomeCta, HomeBottomWidget, HomeTitle, HomeHeroInfo} from './components/Home'
 
 function App() {
 
-  const [hamMenuActive, setHamMenuActive] = useState(false)
+  const [menuActive, setMenuActive] = useState(false);
 
-  function toggleMenu() {
-    setHamMenuActive(oldValue => !oldValue)
+  const toggleMenu = () => {
+    setMenuActive((prevMenuActive) => !prevMenuActive);
+  };
+
+
+  const headerLinks = [
+    { id: 1, url: './index.html', text: 'Home' },
+    { id: 2, url: './index.html#projects', text: 'Projects' },
+    { id: 3, url: './index.html#about', text: 'About' },
+    { id: 4, url: './index.html#contact', text: 'Contact' },
+  ];
+
+  const socialsData = {
+    linkedin : {
+      link : "https://www.linkedin.com/in/nazim-mouza%C3%AF-647a29211/",
+      srcIcon : "./src/assets/img/linkedin.png"
+    },
+    github : {
+      link : "https://github.com/Nostalgeek18",
+      srcIcon : "./src/assets/img/github-icon.png"
+    }
   }
 
 
   return (
     <>
-    <Header menuActive={hamMenuActive} toggleMenu={toggleMenu} />
-    <Home />
+      <Header>
+        <Logo 
+          src="./src/assets/img/avatar4.png"
+          alt="Nazim Mouzai Logo Image">
+            Bob Marley Jr.
+          </Logo> 
+        <Navigation links={headerLinks} />
+        <HamburgerMenu isActive={menuActive} onClick={toggleMenu} />
+        <SmallMenu isActive={menuActive} links={headerLinks} />
+      </Header>
+    <Home>
+        <HomeContent>
+          <HomeTitle>Hey, my name is Bob Marley</HomeTitle>
+          <HomeHeroInfo>
+             I am a passionate frontend developer based in London, UK. My specialities are <strong>React JS</strong> and
+            <strong>Tailwind CSS</strong>,and I love building apps that are delightful to use.
+          </HomeHeroInfo>
+          <HomeCta>See my projects</HomeCta>
+        </HomeContent>
+        <HomeBottomWidget/>
+        <HeroSocials>
+            <HeroSocial social={socialsData.linkedin}/>
+            <HeroSocial social={socialsData.github}/>
+        </HeroSocials>
+    </Home> 
     <Projects />
     <About />
     <Contact />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,77 @@
-export default function Header({menuActive, toggleMenu}) {
 
-    return (
-        <header className="header">
-                <div className="header__content">
-                <div className="header__logo-container">
-                    <div className="header__logo-img-cont">
-                    <img
-                        src="./src/assets/img/avatar4.png"
-                        alt="Nazim Mouzai Logo Image"
-                        className="header__logo-img"
-                    />
-                    </div>
-                    <span className="header__logo-sub">Jane Smith</span>
-                </div>
-                <div className="header__main">
-                    <ul className="header__links">
-                    <li className="header__link-wrapper">
-                        <a href="./index.html" className="header__link"> Home </a>
-                    </li>
-                    <li className="header__link-wrapper">
-                        <a href="./index.html#projects" className="header__link">
-                        Projects
-                        </a>
-                    </li>
-                    <li className="header__link-wrapper">
-                        <a href="./index.html#about" className="header__link">About </a>
-                    </li>
-                    <li className="header__link-wrapper">
-                        <a href="./index.html#contact" className="header__link"> Contact </a>
-                    </li>
-                    </ul>
-                    <div className="header__main-ham-menu-cont" onClick={toggleMenu}>
-                    <img
-                        src="./src/assets/img/ham-menu.svg"
-                        alt="hamburger menu"
-                        className="header__main-ham-menu"
-                    />
-                    <img
-                        src="./assets/svg/ham-menu-close.svg"
-                        alt="hamburger menu close"
-                        className="header__main-ham-menu-close d-none"
-                    />
-                    </div>
-                </div>
-                </div>
-                <div className={`header__sm-menu ${menuActive ? 'header__sm-menu--active' : ''}`}>
-                <div className="header__sm-menu-content">
-                    <ul className="header__sm-menu-links">
-                    <li className="header__sm-menu-link">
-                        <a href="./index.html"> Home </a>
-                    </li>
 
-                    <li className="header__sm-menu-link">
-                        <a href="./index.html#about"> About </a>
-                    </li>
-                    
-                    <li className="header__sm-menu-link">
-                        <a href="./index.html#projects"> Projects </a>
-                    </li>
+// Compound component: Header
+function Header({ children }) {
 
-                    <li className="header__sm-menu-link">
-                        <a href="./index.html#contact"> Contact </a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-        </header>
-    )
-
+  return (
+    <header className="header">
+      <div className="header__content">
+        {children}
+      </div>
+    </header>
+  );
 }
+
+// Compound component: Logo
+function Logo({ src, alt, children }) {
+  return (
+    <div className="header__logo-container">
+      <div className="header__logo-img-cont">
+        <img src={src} alt={alt} className="header__logo-img" />
+      </div>
+      <span className="header__logo-sub">{children}</span>
+    </div>
+  );
+}
+
+// Compound component: Navigation
+function Navigation({ links }) {
+  return (
+    <ul className="header__links">
+      {links.map((link) => (
+        <li className="header__link-wrapper" key={link.id}>
+          <a href={link.url} className="header__link">
+            {link.text}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// Compound component: HamburgerMenu
+function HamburgerMenu({ isActive, onClick }) {
+  return (
+    <div className="header__main-ham-menu-cont" onClick={onClick}>
+      <img
+        src="./src/assets/img/ham-menu-close.svg"
+        alt="hamburger menu"
+        className={`header__main-ham-menu ${isActive ? '' : 'd-none'}`}
+      />
+      <img
+        src="./src/assets/img/ham-menu.svg"
+        alt="hamburger menu close"
+        className={`header__main-ham-menu-close ${isActive ? 'd-none' : ''}`}
+      />
+    </div>
+  );
+}
+
+// Compound component: SmallMenu
+function SmallMenu({ isActive, links }) {
+  return (
+    <div className={`header__sm-menu ${isActive ? 'header__sm-menu--active' : ''}`}>
+      <div className="header__sm-menu-content">
+        <ul className="header__sm-menu-links">
+          {links.map((link) => (
+            <li className="header__sm-menu-link" key={link.id}>
+              <a href={link.url}>{link.text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export { Header, Logo, Navigation, HamburgerMenu, SmallMenu };
